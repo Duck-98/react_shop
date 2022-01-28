6a94a5676eb167160d1dc7bd48985f896d316fe5
duck <godqhr2256@gmail.com>
2022-01-28 16:49:50 +0900

    컴포넌트 생성 및 App.js 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import './App.css';
 import {
   Container,
@@ -10,59 +11,38 @@ import {
 import React, { useState } from 'react';
 import Product from './components/product';
 import data from './data';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './components/detail';
+import Header from './components/header';
 
 function App() {
   const [shoeData, setShoeData] = useState(data);
   return (
     <>
       <div className="App">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Shop</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Header />
 
-        <Jumbotron className="background">
-          <h1 color="white">30% Season Sale</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
-      </div>
-      <div className="container">
-        {shoeData.map((a, i) => {
-          return <Product shoeData={shoeData[i]} i={i} key={i} />;
-        })}
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1 color="white">30% Season Sale</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+          <div className="container">
+            {shoeData.map((a, i) => {
+              return <Product shoeData={shoeData[i]} i={i} key={i} />;
+            })}
+          </div>
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
       </div>
     </>
   );
